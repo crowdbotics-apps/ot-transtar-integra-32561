5f4e3c2ab216e5e2359a561a877bc71e3972c4eb
Crowdbotics <team@crowdbotics.com>
2021-12-23 14:25:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "ot_transtar_integra_32561",
+  url: "https://ot_transtar_integra_32561.botics.co",
+  api: "https://ot_transtar_integra_32561.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
