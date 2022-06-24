fb78e45b8c83f5e6b0728b3447500d44e1da32d7
Emmanuel <emmanuelhemarxyll@gmail.com>
2022-06-24 17:22:36 +0100

    folder restructure

--- frontend/src/screens/broker/FirmUsers/FirmUsers.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { DashboardWrapper } from "../Dashboard/Dashboard.style"
-type Props = {}
-
-const FirmUsers = (props: Props) => {
-  return <DashboardWrapper>Firm Users</DashboardWrapper>
-}
-
-export default FirmUsers
