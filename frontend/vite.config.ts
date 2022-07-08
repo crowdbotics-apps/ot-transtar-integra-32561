740f2343fc545397dbcd5ed1ac6b4fcb3e1652f9
Emmanuel <emmanuelhemarxyll@gmail.com>
2022-07-08 21:24:11 +0100

    use tsConfigPaths plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,6 @@ import path from "path"
 import tsconfigPaths from "vite-tsconfig-paths"
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: [{ find: "@components", replacement: path.resolve(__dirname, "src/components") }]
-  },
   build: {
     outDir: "../backend/web_build/",
     assetsDir: "static"
