6bc1efb7e8e8d8a7c51b12847980b7bf69b8ef81
Emmanuel <emmanuelhemarxyll@gmail.com>
2022-07-17 00:10:43 +0100

    register firm api implementation

--- frontend/src/screens/admin/Dashboard/Firms/RegisterFirm.tsx
+++ frontend/src/screens/admin/Dashboard/Firms/RegisterFirm.tsx
@@ -1,14 +1,34 @@
-import React from "react"
-import RegistrationForm from "../../../../components/Forms/broker/Registration/Registration.form"
+import { useState } from "react"
+import RegistrationForm from "components/Forms/broker/Registration/Registration.form"
+import Spinner from 'components/Spinner'
 import Header from "../../Header"
+import Api from 'Api'
 import { DashboardWrapper } from "../Dashboard.style"
+import { FirmData } from 'types'
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom"
 
 
 const RegisterFirm = () => {
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+  const onSubmit = async (data: FirmData) => {
+    setLoading(true)
+    const res = await Api.create('/company/', data);
+    setLoading(false)
+    if (res) {
+      toast.success('Firm registered successfully', { autoClose: 2000, });
+      navigate(-1)
+      return true;
+    } else {
+      return false
+    }
+  }
   return (
     <DashboardWrapper>
       <Header showBack> </Header>
-      <RegistrationForm header="REGISTER NEW FIRM" />
+      <RegistrationForm header="REGISTER NEW FIRM" onSubmitData={onSubmit} />
+      {loading && <Spinner />}
     </DashboardWrapper>
   )
 }
